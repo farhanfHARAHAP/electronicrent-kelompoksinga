78cad06b848d33fccc6ed7a6a6f63f9f56f1f5b6
farhanfHARAHAP <91046795+farhanfHARAHAP@users.noreply.github.com>
2022-12-11 23:11:15 +0700

    Add files via upload

--- insertdb.sql
+++ insertdb.sql
@@ -4,7 +4,7 @@
 INSERT INTO `barang`(`id_barang`, `nama`, `brand`, `type`, `harga`, `icon_url`, `av_quantity`, `desc`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]');
 
 -- `ledger` table
-INSERT INTO `ledger`(`invoice`, `phone`, `id_barang`, `start_date`, `end_date`, `price`, `returned`, `paid`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]');
+INSERT INTO `ledger`(`invoice`, `phone`, `id_barang`, `start_date`, `end_date`, `price`, `returned`, `paid`, `quantity`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]');
 
 -- `penyewa` table
 INSERT INTO `penyewa`(`phone`, `nama`, `alamat`) VALUES ('[value-1]','[value-2]','[value-3]');
